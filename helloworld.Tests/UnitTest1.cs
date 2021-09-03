ede9cad5323d8979a2cb2fe78c3c7653a41e98af
KusMar00 <74660130+KusMar00@users.noreply.github.com>
2021-09-03 13:30:25 +0200

    implement helloworld (#1)

--- helloworld.Tests/UnitTest1.cs
+++ helloworld.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace helloworld.Tests
@@ -6,9 +7,18 @@ namespace helloworld.Tests
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Main_writes_hello_world()
         {
+            //arrange
+            var writer = new StringWriter();
+            Console.SetOut(writer);
 
+            //act
+            Program.Main(new string[0]);
+            var output = writer.GetStringBuilder().ToString().Trim();
+
+            //assert
+            Assert.Equal("Hello, World!", output);
         }
     }
 }
